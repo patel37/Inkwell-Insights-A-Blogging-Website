af09eaa48d2e18f22d04c972e7db36ec124ec4b4
jaydchavan007 <jaydchavan007@gmail.com>
2024-03-05 19:04:25 +0530

    profile pic added

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,14 +1,28 @@
 import React from 'react'
+import { useState  } from 'react'
 import {useSelector} from 'react-redux'
 import { TextInput,Button } from 'flowbite-react'
+
 const DashProfile = () => {
     const {currentUser}=useSelector(state=>state.user)
-  return (
+    const [imageFile, setImageFile] = useState(null);
+    const [imageFileUrl, setImageFileUrl ] = useState(null);
+    const handleImageChange = (e) => {
+        const file = e.target.files[0]; 
+        if(file){
+            setImageFile(file);  
+            setImageFileUrl(URL.createObjectURL(file  ));   
+        }
+    }; 
+    console.log(imageFile,imageFileUrl );
+    return (
     <div className='max-w-lg mx-auto p-3 w-full'>
         <h1 className='my-7 text-center font-semibold text-3xl '>Profile</h1>
         <form className='flex flex-col gap-4' >
+            <input type="file" accept='image/*' onChange={handleImageChange}/>
             <div className='w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full'>
-            <img src={currentUser.profilePicture} alt="user" className='rounded-full w-full h-full border-8 border-[lightgray]' />
+            <img
+             src={imageFileUrl || currentUser.profilePicture} alt="user" className='rounded-full w-full h-full border-8 border-[lightgray]' />
             </div>
             <TextInput type='text' id='username' placeholder='username' defaultValue={currentUser.username}/>
             <TextInput type='email' id='email' placeholder='email' defaultValue={currentUser.email}/>
--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -22,11 +22,12 @@ const DashSidebar = () => {
     <Sidebar className='w-full md:w-56'>
       <Sidebar.Items>
         <Sidebar.ItemGroup>
-            <Link to='/dashboard?tab-profile'>
-          <Sidebar.Item active={tab==='profile'} icon={HiUser} label={'User'} labelColor="dark">
+        <Link to='/dashboard?tab-profile'>
+          <Sidebar.Item active={tab==='profile'} icon={HiUser} label={'User'} labelColor="dark" as='div'>
             Profile
           </Sidebar.Item>
           </Link>
+
           <Sidebar.Item
             icon={HiArrowSmRight}
             label={'User'}
