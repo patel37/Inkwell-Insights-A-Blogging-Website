af147fac522506a0af4dc7d03d26f42ac48fb3a9
jaydchavan007 <jaydchavan007@gmail.com>
2024-03-09 00:40:42 +0530

    userimageupload

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import { useRef,  useState, useEffect } from 'react'
 import {useSelector} from 'react-redux'
 import { TextInput,Button } from 'flowbite-react'
-import{ getStorage, ref, uploadBytesResumable } from "firebase/storage";
-import{ app} from '../firebase';
-import{ getDownloadURL } from 'firebase/storage';
-
+import{ app } from '../firebase';
+import{ getDownloadURL, getStorage, ref, uploadBytesResumable} from 'firebase/storage';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
 const DashProfile = () => {
     const {currentUser}=useSelector(state=>state.user)
@@ -56,18 +56,38 @@ const DashProfile = () => {
         <h1 className='my-7 text-center font-semibold text-3xl '>Profile</h1>
         <form className='flex flex-col gap-4' >
             <input type="file" accept='image/*' onChange={handleImageChange} ref={filePickerRef} hidden/> 
-            <div className='w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full' 
+            <div className='relative w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full' 
             onClick={() => filePickerRef.current.click( )}>
+                {imageFileUploadProgress && (
+                    <CircularProgressbar value={imageFileUploadProgress || 0} 
+                    text={`${imageFileUploadProgress}%`}
+                    strokeWidth={5}
+                    styles={{
+                        root:{
+                            width: '100%',
+                            height: '100%',
+                            position: '100%',
+                            top: 0,
+                            left: 0,
+                        
+                        },
+                        path:{
+                            stroke: `rgba(62, 152, 199, ${imageFileUploadProgress /100})`,
+                        }
+                    }}
+                    />
+                )}
             <img
-             src={imageFileUrl || currentUser.profilePicture} alt="user" className='rounded-full w-full h-full border-8 border-[lightgray]' />
+             src={imageFileUrl || currentUser.profilePicture} alt="user" className='rounded-full w-full h- object-cover border-8 border-[lightgray]' />
             </div>
+            {imageFileUploadError && <Alert color='failure'>{imageFileUploadError}</Alert>}
             <TextInput type='text' id='username' placeholder='username' defaultValue={currentUser.username}/>
             <TextInput type='email' id='email' placeholder='email' defaultValue={currentUser.email}/>
             <TextInput type='password' id='password' placeholder='username'/>
             <Button type='submit' gradientDuoTone='purpleToBlue' outline>
                 Update
             </Button>
-        </form>
+        </form> 
         <div className='text-red-500 flex justify-between mt-5'>
             <span className='cursor-pointer'>Delete Account</span>
             <span className='cursor-pointer'>Sign Out</span>
