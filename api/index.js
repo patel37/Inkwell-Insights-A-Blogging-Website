d637bc4006ffe2d43041f2fad4c3a0b29fd129eb
nithin <sureshnithin1729@gmail.com>
2024-03-11 00:37:34 +0530

    update user api route

--- api/index.js
+++ api/index.js
@@ -12,9 +12,11 @@ mongoose.connect(process.env.MONGO)
 })
 import userRoutes from './routes/user.route.js'
 import authRoutes from './routes/auth.route.js'
+import cookieParser from 'cookie-parser';
 
 const app=express();
 app.use(express.json());
+app.use(cookieParser());
 app.listen(3000,()=>{
     console.log("Server is running  on port 3000!");
 });
