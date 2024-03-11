07d86656233d6a911c7203d5392863b23c010ffe
jaydchavan007 <jaydchavan007@gmail.com>
2024-03-12 01:38:35 +0530

    add create a post page ui

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,7 +1,7 @@
 import { Alert, Button, Modal, ModalBody, TextInput } from 'flowbite-react';
 import { useEffect, useRef, useState } from 'react';
 import { useSelector } from 'react-redux';
-import {
+import { 
   getDownloadURL,
   getStorage,
   ref,
@@ -21,9 +21,10 @@ import {
 } from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
 import { HiOutlineExclamationCircle } from 'react-icons/hi';
+import { Link } from 'react-router-dom';
 
 export default function DashProfile() {
-  const { currentUser, error } = useSelector((state) => state.user);
+  const { currentUser, error, loading } = useSelector((state) => state.user);
   const [imageFile, setImageFile] = useState(null);
   const [imageFileUrl, setImageFileUrl] = useState(null);
   const [imageFileUploadProgress, setImageFileUploadProgress] = useState(null);
@@ -232,9 +233,26 @@ export default function DashProfile() {
           placeholder='password'
           onChange={handleChange}
         />
-        <Button type='submit' gradientDuoTone='purpleToBlue' outline>
-          Update
+        <Button 
+        type='submit' 
+        gradientDuoTone='purpleToBlue' 
+        outline 
+        disabled={loading || imageFileUploading}>
+          
+          {loading ? 'Loading...' : 'Update'}
         </Button>
+        { currentUser.isAdmin && (
+          <Link to={'/create-post'}>
+            <Button 
+              type='button'
+              gradientDuoTone='purpleToPink'
+              className='w-full'
+              >
+                Create a post
+              </Button>
+              </Link>
+           )
+        }
       </form>
       <div className='text-red-500 flex justify-between mt-5'>
         <span onClick={() => setShowModal(true)} className='cursor-pointer'>
