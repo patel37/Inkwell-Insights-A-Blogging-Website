1d50674d7c2d38424c5b9dc5e93aff67b706259f
nithin <sureshnithin1729@gmail.com>
2024-03-16 19:42:28 +0530

    made alters

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -19,9 +19,9 @@ const FooterCom = () => {
               className="self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white"
             >
               <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
-                Ethereal
+                Inkwell
               </span>
-              Blog
+              Insights
             </Link>
           </div>
           <div className="grid grid-cols-2 gap-8 mt-4 sm:grid-cols-3 sm:gap-6">
@@ -40,7 +40,7 @@ const FooterCom = () => {
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  Ethereal Blog
+                  Inkwell Insights
                 </Footer.Link>
               </Footer.LinkGroup>
             </div>
@@ -88,14 +88,14 @@ const FooterCom = () => {
         <div className="w-full sm:flex sm:items-center sm:justify-between">
           <Footer.Copyright
             href=""
-            by="Ethereal Blog"
+            by="Inkwell Insights"
             year={new Date().getFullYear()}
           />
           <div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center">
             <Footer.Icon href="#" icon={BsFacebook} />
             <Footer.Icon href="#" icon={BsInstagram} />
             <Footer.Icon href="#" icon={BsTwitter} />
-            <Footer.Icon href="#" icon={BsGithub} />
+            <Footer.Icon href="https://github.com/Nithin1729S/Blogging-Website" icon={BsGithub} />
             <Footer.Icon href="#" icon={BsReddit} />
           </div>
         </div>
