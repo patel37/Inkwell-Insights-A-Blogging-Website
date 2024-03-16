1cfcc4589e351c9a86cefc822ed243a6df03c262
nithin <sureshnithin1729@gmail.com>
2024-03-16 17:34:01 +0530

    Search Feature added

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,5 +1,26 @@
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <div className='min-h-screen flex items-center justify-center'>
+      <div className='max-w-2xl mx-auto p-3 text-center'>
+        <div>
+          <h1 className='text-3xl font font-semibold text-center my-7'>
+            About Ethereal Blog
+          </h1>
+          <div className='text-md text-gray-500 flex flex-col gap-6'>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis cupiditate, reprehenderit corporis quis dignissimos quaerat esse est delectus. Ab obcaecati quidem pariatur nihil omnis doloremque similique. Similique nemo laudantium sapiente aspernatur ex temporibus iste asperiores odio, voluptatibus minus illum nulla architecto adipisci modi fuga? Voluptates repellendus dolorem sit tempore maxime praesentium excepturi quisquam atque sunt magnam, fuga laboriosam hic sapiente officiis amet aliquam quas quos maiores perferendis! Repudiandae, in facilis?
+            </p>
+
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed in debitis unde odio tempora numquam quae obcaecati! A quo asperiores tempora veritatis nulla commodi sequi nostrum corporis id placeat odio quis nam, in eos atque porro earum magnam dolore maxime.
+            </p>
+
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae vel nihil repellat quasi blanditiis, voluptas nulla architecto facere voluptatum. Doloremque, quod esse quia numquam nostrum maiores quibusdam molestiae voluptatum deleniti odio praesentium. Quia qui odio modi quam nisi laudantium tempora nobis ea!
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
